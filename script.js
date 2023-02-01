1bda11e89c70a0c6e9947d6d56e8fc84a54ea7fe
cosmos hagan <cosmoshagan@cosmoss-MacBook-Pro.local>
2023-02-01 19:49:29 +0000

    Update script.js files for show mobile menu

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // ─── Selectors ───────────────────────────────────────────────────────────────
 const mobileMenu = document.querySelector('.mobile-menu');
-const navlinks = document.querySelector('.nav-links');
+const navLinks = document.querySelector('.nav-links');
 const navBtn = document.querySelectorAll('.nav-links ul li a');
 
 // ─── Function ────────────────────────────────────────────────────────────────
 function showMenu() {
-  navlinks.classList.toggle('show-menu');
+  navLinks.classList.toggle('show-menu');
   if (mobileMenu.src.match('./images/IconMenu.png')) {
     mobileMenu.src = './images/IconCancel.png';
   } else {

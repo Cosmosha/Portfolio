544f6db518b89ccfbbca263f83d20d81f698cc71
cosmos hagan <cosmoshagan@cosmoss-MacBook-Pro.local>
2023-02-03 18:30:57 +0000

    Fix eslint error in style.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
+// ─── Works Data ──────────────────────────────────────────────────────────────
+
+// Projects Database
+const data = [{
+  id: '1',
+  imageSrc: './images/Portfolio.png',
+  title: 'Multi-Post Stories',
+  outline: ['FACEBOOK', 'Full Stack Dev', 'JavaScript'],
+  tech: ['html', 'css', 'javascript'],
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod quis nostrud exercitation ullamco laboris nisi.',
+  liveLink: 'https://cosmosha.github.io',
+  sourceLink: '#',
+},
+{
+  id: '2',
+  imageSrc: './images/Portfolio4.png',
+  title: 'Facebook 360',
+  outline: ['FACEBOOK', 'Full Stack Dev', 'JavaScript'],
+  tech: ['html', 'css', 'javascript'],
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  liveLink: 'https://cosmosha.github.io',
+  sourceLink: '#',
+},
+{
+  id: '3',
+  imageSrc: './images/Portfolio2.png',
+  title: 'Tonic',
+  outline: ['FACEBOOK', 'Full Stack Dev', '2016'],
+  tech: ['html', 'css', 'javascript'],
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  liveLink: 'https://cosmosha.github.io',
+  sourceLink: '#',
+},
+{
+  id: '4',
+  imageSrc: './images/Portfolio3.png',
+  title: 'Uber Navigation',
+  outline: ['Uber', 'Lead Developer', '2018'],
+  tech: ['html', 'css', 'javascript'],
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  liveLink: 'https://cosmosha.github.io',
+  sourceLink: '#',
+},
+];
+
 // ─── Selectors ───────────────────────────────────────────────────────────────
 const mobileMenu = document.querySelector('.mobile-menu');
 const navlinks = document.querySelector('.nav-links');
@@ -7,31 +52,22 @@ const workSection = document.querySelector('.works');
 
 const popUp = document.querySelector('.popup');
 
-
-
-
-
-
-
-
 // ─── Function ────────────────────────────────────────────────────────────────
 
 // ─── Mobile Menu ─────────────────────────────────────────────────────────────
 function showMenu() {
-    navlinks.classList.toggle('show-menu');
-    if (mobileMenu.src.match('./images/IconMenu.png')) {
-        mobileMenu.src = './images/IconCancel.png';
-    } else {
-        mobileMenu.src = './images/IconMenu.png';
-    }
+  navlinks.classList.toggle('show-menu');
+  if (mobileMenu.src.match('./images/IconMenu.png')) {
+    mobileMenu.src = './images/IconCancel.png';
+  } else {
+    mobileMenu.src = './images/IconMenu.png';
+  }
 }
 
 // ─── Render Works Section ────────────────────────────────────────────────────
 function renderWorkSection() {
-
-    for (let i = 0; i < data.length; i++) {
-
-        workSection.insertAdjacentHTML('beforeend', `
+  for (let i = 0; i < data.length; i += 1) {
+    workSection.insertAdjacentHTML('beforeend', `
         <div class="grid-container">
         <div class="mobile">
             <!-- ─── Works Project 4 ─────────────────────────── -->
@@ -105,20 +141,18 @@ function renderWorkSection() {
       
         </div>
       </div>
-        `)
-    }
+        `);
+  }
 
+  // ─── Display Popup ───────────────────────────────────────────────────────────
 
-    // ─── Display Popup ───────────────────────────────────────────────────────────
+  const btnPop = document.querySelectorAll('.popBtn');
 
-    const btnPop = document.querySelectorAll('.popBtn');
-
-
-    function showPopUP(currentBtn) {
-        const dataId = +currentBtn;
-        const projectID = data[dataId - 1];
-        popUp.classList.add('popup-show');
-        popUp.insertAdjacentHTML('beforeend', `
+  function showPopUP(currentBtn) {
+    const dataId = +currentBtn;
+    const projectID = data[dataId - 1];
+    popUp.classList.add('popup-show');
+    popUp.insertAdjacentHTML('beforeend', `
         <div class="works-container">
         <div class="popup-header">
             <div class="project-title">
@@ -156,8 +190,8 @@ function renderWorkSection() {
                 <hr>
                 <div class="action ">
                     <div class="btnPosition">
-                        <button type="button" class="btn"><a href="${projectID.liveLink}"></a>See live <img src="./images/iconExport.png" class="btn-icon" alt="button icom"></button>
-                        <button type="button" class="btn"><a href="${projectID.sourceLink}"></a>See Source <img src="./images/iconGitHub.png" class="btn-icon" alt=" btn-icon"></button>
+                        <button type="button" class="btn"><a href="${projectID.liveLink}">See live <img src="./images/iconExport.png"></a></button>
+                        <button type="button" class="btn"><a href="${projectID.sourceLink}">See Source <img src="./images/iconGitHub.png"></a></button>
                     </div>
                 </div>
             </div>
@@ -166,32 +200,20 @@ function renderWorkSection() {
         </div>
     
     </div>
-        `)
+        `);
 
-        const closePopup = document.getElementById('closePopup');
+    const closePopup = document.getElementById('closePopup');
 
-        function closePopUp() {
-            popUp.classList.remove('popup-show');
-            popUp.innerHTML = '';
-        }
-
-
-        closePopup.addEventListener('click', closePopUp);
-
+    function closePopUp() {
+      popUp.classList.remove('popup-show');
+      popUp.innerHTML = '';
     }
 
-    btnPop.forEach(btn => btn.addEventListener('click', (e) => { showPopUP(e.target.dataset.id) }));
+    closePopup.addEventListener('click', closePopUp);
+  }
+
+  btnPop.forEach((btn) => btn.addEventListener('click', (e) => { showPopUP(e.target.dataset.id); }));
 }
-
-
-
-
-
-
-
-
-
-
 
 // ─── Event Listeners ──────────────────────────────────────────────────────────
 window.onload = renderWorkSection;
@@ -199,5 +221,5 @@ window.onload = renderWorkSection;
 mobileMenu.addEventListener('click', showMenu);
 
 navBtn.forEach((link) => {
-    link.addEventListener('click', showMenu);
+  link.addEventListener('click', showMenu);
 });
